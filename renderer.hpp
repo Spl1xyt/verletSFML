18f9d4f609dca68ccf5a5466c8e6ee68c3683bdf
Spl1xyt <103865074+Spl1xyt@users.noreply.github.com>
2025-05-13 09:39:34 +0200

    Really removed the rendering of the grid on screen

--- renderer.hpp
+++ renderer.hpp
@@ -25,16 +25,6 @@ class Renderer {
         circle.setScale({obj.radius, obj.radius});
         circle.setFillColor(obj.color);
         window.draw(circle);
-
-
-        // Temp to introduce grid partition
-        //
-        //
-        // text.setString(std::to_string(obj.id));
-        // text.setPosition({obj.position.x - 10.0f, obj.position.y - 10.0f});
-        // text.setCharacterSize(15);
-        // text.setFillColor(sf::Color::White);
-        // window.draw(text);
       }
 
       // Display amount of particles in the simulations
@@ -50,21 +40,6 @@ class Renderer {
       text.setPosition({20.0f, 40.0f});
       text.setFillColor(sf::Color::White);
       window.draw(text);
-
-      // Temp to introduce grid partition
-      //
-      //
-      // for (int i = 0; i < 840 / 20; i++){
-      //   for (int j = 0; j < 840 / 20; j++){
-      //     sf::RectangleShape rect({20.0f, 20.0f});
-      //     rect.setPosition({i * 20.0f, j * 20.0f});
-      //     rect.setOutlineColor(sf::Color::White);
-      //     rect.setOutlineThickness(0.5f);
-      //     rect.setFillColor(sf::Color::Transparent);
-      //     window.draw(rect);
-      //   }
-      // }
-
     }
 
   private:
