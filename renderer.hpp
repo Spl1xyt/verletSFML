692cc29ae08318c2899dbfe48d1ef7904c7b0316
Spl1xyt <103865074+Spl1xyt@users.noreply.github.com>
2025-05-13 09:03:36 +0200

    Implement grid partitioning for better performance

--- renderer.hpp
+++ renderer.hpp
@@ -13,6 +13,8 @@ class Renderer {
     void render(Solver& solver, int fps){
       const auto& objects = solver.getObjects();
       std::string num_objects = std::to_string(objects.size());
+      sf::Font font("arial.ttf");
+      sf::Text text(font);
 
       // Display the particles
       sf::CircleShape circle(1.0f);
@@ -23,11 +25,19 @@ class Renderer {
         circle.setScale({obj.radius, obj.radius});
         circle.setFillColor(obj.color);
         window.draw(circle);
+
+
+        // Temp to introduce grid partition
+        //
+        //
+        text.setString(std::to_string(obj.id));
+        text.setPosition({obj.position.x - 10.0f, obj.position.y - 10.0f});
+        text.setCharacterSize(15);
+        text.setFillColor(sf::Color::White);
+        window.draw(text);
       }
 
       // Display amount of particles in the simulations
-      sf::Font font("arial.ttf");
-      sf::Text text(font);
       text.setString("Objects : " + num_objects);
       text.setCharacterSize(20);
       text.setPosition({20.0f, 20.0f});
@@ -40,6 +50,20 @@ class Renderer {
       text.setPosition({20.0f, 40.0f});
       text.setFillColor(sf::Color::White);
       window.draw(text);
+
+      // Temp to introduce grid partition
+      //
+      //
+      for (int i = 0; i < 840 / 20; i++){
+        for (int j = 0; j < 840 / 20; j++){
+          sf::RectangleShape rect({20.0f, 20.0f});
+          rect.setPosition({i * 20.0f, j * 20.0f});
+          rect.setOutlineColor(sf::Color::White);
+          rect.setOutlineThickness(0.5f);
+          rect.setFillColor(sf::Color::Transparent);
+          window.draw(rect);
+        }
+      }
 
     }
 
